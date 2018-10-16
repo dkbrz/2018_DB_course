0526141263edc34b0f7fbb2006b66c6b1730f640
dkbrz <glaz.dikobraz@gmail.com>
2018-10-16 21:47:51 +0300

    HW_1

---
+++ HW_1/app.py
@@ -0,0 +1,100 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+from patterns import *
+
+import sqlite3
+
+class Work(object):
+    def __init__(self, args):
+        self.args = args
+    
+    def result(self):
+        con = sqlite3.connect('test.db')
+        cur = con.cursor()
+        string = '1=1'
+        for i in ['id_test', 't_name', 'discipline','c_date']:
+            if self.args[i] != '':
+                string = string + ' AND ' + WORK[self.args['TYPE']][1][i].format(self.args[i])
+        cur.execute(WORK[self.args['TYPE']][0].format(string))
+        results = ['</td><td>'.join(str(j) for j in i) for i in [WORK[self.args['TYPE']][2]]+cur.fetchall()]
+        
+        html_part='<table>'
+        for t in results:
+            html_part=html_part + '<tr><td>' + t + '</td></tr>'
+        html_part=html_part + '</table>'
+        return html_part
+
+class Student(object):
+    def __init__(self, args):
+        self.args = args
+    
+    def result(self):
+        con = sqlite3.connect('test.db')
+        cur = con.cursor()
+        string = '1=1'
+        for i in ['id_student', 's_name', 's_course', 's_degree', 's_program']:
+            if self.args[i] != '':
+                string = string + ' AND ' + STUDENT[self.args['TYPE']][1][i].format(self.args[i])
+        cur.execute(STUDENT[self.args['TYPE']][0].format(string))
+        results = ['</td><td>'.join(str(j) for j in i) for i in [STUDENT[self.args['TYPE']][2]]+cur.fetchall()]
+        
+        html_part='<table>'
+        for t in results:
+            html_part=html_part + '<tr><td>' + t + '</td></tr>'
+        html_part=html_part + '</table>'
+        return html_part
+    
+        return results
+        return [STUDENT[self.args['TYPE']][0].format(string)]
+            
+
+@app.route('/',  methods=['GET'])
+def index():
+    if request.args:
+        if request.args['what']=='TEST':
+            query = Work(request.args)
+        else:
+            query = Student(request.args)
+        result = query.result()
+        return render_template('index.html', result=result)
+    return render_template('index.html',result=[])
+
+@app.route('/add',  methods=['GET'])
+def add():
+    con = sqlite3.connect('test.db')
+    cur = con.cursor()
+    if request.args:
+        if request.args['what'] == 'TEST':
+            cur.execute(ADD['TEST'].format(request.args['discipline'],
+                                           request.args['task_text'],
+                                           request.args['t_name'],
+                                           request.args['c_date']))
+            con.commit()
+        elif request.args['what'] == 'STUDENT':
+            cur.execute(ADD['STUDENT'].format(request.args['s_name'],
+                                           str(request.args['s_course']),
+                                           request.args['s_degree'],
+                                           request.args['s_program']))
+            con.commit()
+        elif request.args['what'] == 'RESULTS':
+            cur.execute(ADD['RESULTS'].format(str(request.args['id_student']),
+                                           str(request.args['id_test']),
+                                           str(request.args['mark'])))
+            con.commit()
+        return render_template('add.html', result=['Результат успешно добавлен'])
+    return render_template('add.html',result=[])
+
+@app.route('/edit',  methods=['GET'])
+def edit():
+    if request.args:
+        
+        return render_template('edit.html', result=result)
+    return render_template('edit.html',result=[])
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5005, debug=True)
+    
---
+++ HW_1/patterns.py
@@ -0,0 +1,21 @@
+WORK = {'INFO' : ['SELECT tests.* FROM tests WHERE {} ORDER BY discipline, c_date, id_test', 
+       {'id_test':'tests.id_test = {}', 't_name':'tests.t_name = \"{}\"', 'discipline':'tests.discipline = \"{}\"',
+       'c_date':'tests.c_date = \"{}\"'},
+                 ['<b>ID теста</b>','<b>Дисциплина</b>','<b>Название теста</b>','<b>Задание</b>']],
+
+        'RESULTS': ['SELECT DISTINCT tests.discipline, tests.t_name, tests.c_date, results.mark, students.s_name, students.s_course, students.s_degree, students.s_program FROM students, results, tests WHERE results.id_test=tests.id_test AND results.id_student = students.id_student AND {} ORDER BY tests.discipline, tests.c_date, tests.id_test, students.s_degree, students.s_course, students.s_name',
+        {'id_test':'tests.id_test = {}', 't_name':'tests.t_name = \"{}\"', 'discipline':'tests.discipline = \"{}\"'},
+                   ['<b>Дисциплина</b>','<b>Название</b>','<b>Дата проведения</b>','<b>Оценка</b>','<b>Имя</b>','<b>Курс</b>','<b>Степень</b>','<b>Программа</b>']]}
+
+STUDENT = {'INFO' : ['SELECT DISTINCT students.* FROM students WHERE {} ORDER BY students.s_program, students.s_degree, students.s_course, students.s_name', 
+       {'id_student':'students.id_student = {}','s_course':'students.s_course = {}','s_name':'students.s_name = \"{}\"','s_program':'students.s_program = \"{}\"','s_degree':'students.s_degree = \"{}\"'},
+                    ['<b>ID</b>','<b>Фамилия, имя</b>','<b>Курс</b>','<b>Степень</b>','<b>Программа</b>']],
+        
+        'RESULTS': ['SELECT DISTINCT tests.id_test, tests.discipline, tests.t_name, tests.c_date, results.mark FROM tests, students, results WHERE results.id_student=students.id_student AND results.id_test=tests.id_test AND {} ORDER BY tests.discipline, tests.c_date',
+        {'id_student':'students.id_student = {}','s_course':'students.s_course = {}','s_name':'students.s_name = \"{}\"','s_program':'students.s_program = \"{}\"','s_degree':'students.s_degree = \"{}\"'},
+                   ['<b>ID теста</b>','<b>Дисциплина</b>','<b>Название</b>','<b>Дата</b>','<b>Оценка</b>']]}
+
+ADD = {'STUDENT':'INSERT INTO students (s_name, s_course, s_degree, s_program) VALUES (\"{}\", {}, \"{}\",\"{}\")',
+       'TEST' :'INSERT INTO tests (discipline, task_text, t_name, c_date) VALUES (\"{}\", \"{}\", \"{}\",\"{}\")',
+       'RESULTS':'INSERT INTO results (id_student, id_test, mark) VALUES ({},{},{})'}
+
